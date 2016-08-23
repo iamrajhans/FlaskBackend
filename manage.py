0c9794a1a76f621ff6426b4d9ccf0c6efb4199b0
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-24 00:41:05 +0530

    added migrate command for db using flask_migrate

--- manage.py
+++ manage.py
@@ -1,16 +1,18 @@
-from drone.main import create_app
+from drone.main import create_app,db
 from flask_script import Manager,Server
-from flask_migrate import Migrate 
+from flask_migrate import Migrate,MigrateCommand
 
 #setup your app
 def setup_app(config):
     app =create_app(config)
+    migrate.init_app(app,db)
     #migrate
     return app
-
+migrate =Migrate()
 
 manager = Manager(setup_app,help='Manage script')
 manager.add_command("runserver",Server())
+manager.add_command("db",MigrateCommand())
 manager.add_option('--app-config',dest='config',default='local',help='Application config load ')
 
 if __name__ == "__main__":
