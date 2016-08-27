8adfb32faf20444c7979bbd4d31e2e5e221ac1ab
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-27 13:00:01 +0530

    added sentry dsn

--- drone/config/common.py
+++ drone/config/common.py
@@ -1,5 +1,11 @@
 #include your common config to your app
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY ='seceret key'
 
 #------- Database URI ----------#
 SQLALCHEMY_DATABASE_URI= 'postgresql://username:password@localhost:5432/databasename'
+
+#-------- Sentry ------#
+
+SENTRY_DSN ='add your dsn sentry here'
