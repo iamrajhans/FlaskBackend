e89ef2af57ce2c8b23d05050f9aaa4159d7eaeaa
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-28 02:04:34 +0530

    api to get the user list name

--- drone/api_start/endpoint.py
+++ drone/api_start/endpoint.py
@@ -6,3 +6,15 @@ from drone.models import UserModel
 def start():
     count = db.session.query(UserModel).count()
     return "Count of users from psql "+str(count)
+
+@api.route('/users',methods=['GET'])
+def users():
+    totalUsers = []
+    users = db.session.query(UserModel).all()
+
+#----- add to list the users -----#
+    for user in users :
+        totalUsers.append(user.name)
+
+    return str(totalUsers)
+
