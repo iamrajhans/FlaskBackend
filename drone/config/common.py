a83edb2b08a598f32456a0cf4f8f4251ae4f974e
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-27 13:00:55 +0530

    made debugging is true

--- drone/config/common.py
+++ drone/config/common.py
@@ -3,6 +3,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY ='seceret key'
 
+DEBUG = True
+
 #------- Database URI ----------#
 SQLALCHEMY_DATABASE_URI= 'postgresql://username:password@localhost:5432/databasename'
 
