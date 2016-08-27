259a3d94834f90d651afd2ac6a275c6888eeb782
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-27 12:03:21 +0530

    showing count of user on initial  endpoint

--- drone/api_start/endpoint.py
+++ drone/api_start/endpoint.py
@@ -1,5 +1,8 @@
 from drone.api_start import api
+from drone.main import db
+from drone.models import UserModel
 
 @api.route('/',methods=['GET'])
 def start():
-    return "Hello World"
+    count = db.session.query(UserModel).count()
+    return "Count of users from psql "+str(count)
