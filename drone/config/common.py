f86fbaf3565ae0a1a77b296bf5681ca6aad70952
iamrajhans <rajhans.jadhao@gmail.com>
2016-08-27 12:07:08 +0530

    added database uri

--- drone/config/common.py
+++ drone/config/common.py
@@ -1,1 +1,5 @@
 #include your common config to your app
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+#------- Database URI ----------#
+SQLALCHEMY_DATABASE_URI= 'postgresql://username:password@localhost:5432/databasename'
