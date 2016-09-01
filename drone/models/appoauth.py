619dd4debdd965954b147a39642b213003cba0f4
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-02 00:31:13 +0530

    added column in app authentication table

---
+++ drone/models/appoauth.py
@@ -0,0 +1,16 @@
+from drone.main import db
+from datetime import datetime
+
+class AppAuthentication(db.Model):
+    __tablename__ = 'app_authentication'
+
+    id       = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(150))
+    api_key  = db.Column(db.String(150))
+
+    def as_dict(self):
+        return {c.name: datetime.strftime(getattr(self, c.name), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.name),datetime) else getattr(self,c.name)for c in self.__table__.columns}
+
+    def __repr__(self):
+        return "<AppAuthentication ('%s')>" % (self.api_key)
