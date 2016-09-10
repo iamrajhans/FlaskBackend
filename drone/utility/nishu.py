e05ef0202b4f337e9cfe4b0fd498ebd8183f71ef
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-10 23:29:53 +0530

    method to store UserModel data

--- drone/utility/nishu.py
+++ drone/utility/nishu.py
@@ -1,9 +1,18 @@
 from drone.main import db
-from drone.models import AppAuthentication
+from drone.models import AppAuthentication,UserModel
 
 
 def get_application_model(api_key):
 
     return db.session.query(AppAuthentication).filter_by(api_key=api_key).first()
 
+def add_user_in_db(data):
+    add_user = UserModel(
+        id=data['id'],
+        name=data['name'],
+        email=data['email']
+    )
+
+    db.session.add(add_user)
+    db.session.flush()
 
