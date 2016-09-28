9af0e5bc00cbbc1f6862a590cfe2e440bf5fb04a
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-29 00:59:19 +0530

    added hmac

--- drone/config/common.py
+++ drone/config/common.py
@@ -1,14 +1,19 @@
 #include your common config to your app
 import logging
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SECRET_KEY ='seceret key'
+
+SECRET_KEY ='secret key'
+
 
 DEBUG = True
 
+
 #------- Database URI ----------#
-SQLALCHEMY_DATABASE_URI= 'postgresql://username:password@localhost:5432/databasename'
+SQLALCHEMY_DATABASE_URI= 'postgresql://Rajhans:laviraj123@localhost:5432/flaskmovie'
+
 
 #-------- Sentry ------#
 
-DSN_SENTRY ='yoour sentry dsn here'
+
+DSN_SENTRY ='https://8bde72b5d31d49b497f1e49226ae0f96:71b0bc035d684846803e5e6ef725ca96@app.getsentry.com/94768'
--- drone/utility/nishu.py
+++ drone/utility/nishu.py
@@ -2,6 +2,7 @@ from drone.main import db
 from drone.models import AppAuthentication,UserModel
 from os import urandom
 import bcrypt
+import hmac
 def get_application_model(api_key):
 
     return db.session.query(AppAuthentication).filter_by(api_key=api_key).first()
