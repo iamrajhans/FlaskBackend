201d34b82e3d81899aea0ce19fb504f228ddfa91
iamrajhans <rajhans.jadhao@gmail.com>
2016-09-30 00:04:50 +0530

    autoincrement in users table

--- drone/models/user.py
+++ drone/models/user.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class UserModel(db.Model):
     __tablename__ = 'users'
-    id    = db.Column(db.Integer,primary_key=True)
+    id    = db.Column(db.Integer,primary_key=True,autoincrement=True)
     name  = db.Column(db.String(100),nullable=True)
     email = db.Column(db.String(150),nullable=True)
 
