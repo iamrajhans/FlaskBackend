1c0ff040a85ab7c784bbb3b9936e573e5e777e43
iamrajhans <rajhans.jadhao@gmail.com>
2016-10-15 22:58:38 +0530

    change in the import sequence in nishu.py

--- drone/utility/nishu.py
+++ drone/utility/nishu.py
@@ -1,7 +1,8 @@
-from drone.main import db
-from drone.models import AppAuthentication,UserModel
 from os import urandom
 import bcrypt
+from drone.main import db
+from drone.models import AppAuthentication,UserModel
+
 def get_application_model(api_key):
 
     return db.session.query(AppAuthentication).filter_by(api_key=api_key).first()
