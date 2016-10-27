0e2fc3ce8be6fa1ee2a6505af6f0b601f398bc4b
iamrajhans <rajhans.jadhao@gmail.com>
2016-10-27 22:37:21 +0530

    seprate file for post api

---
+++ drone/api_start/post_api.py
@@ -0,0 +1,37 @@
+from flask import request,jsonify
+from drone.api_start import api
+from drone.utility.auth_required import auth_decorator
+from drone.utility.nishu import add_user_in_db,set_user_credentials,authenticate_user
+
+
+@api.route('/users',methods=['POST'])
+@auth_decorator
+def insert_user():
+    data = request.get_json()
+
+    if not data or 'id' not in data :
+        return "Data is not provided ",414
+
+    add_user_in_db(data)
+    return jsonify({'result':'success','message':'user is created successfully'}),200
+
+@api.route('/sign_up',methods=['POST'])
+def new_User():
+    user = request.get_json()
+
+    if not user or 'username' and 'password' not in user :
+        return "Data is not Valid",414
+
+    api_key = set_user_credentials(user)
+    return jsonify({'result':'success','data':{'key':api_key}}),200
+
+@api.route('/login',methods=['POST'])
+def login():
+    user = request.get_json()
+
+    if not user or 'username' and 'password' not in user :
+        return "Invalid Data",412
+    name=user['username']
+    passwd=user['password']
+    authenticate_user(name,passwd)
+    return jsonify({'result': 'success', 'message': 'login successful'}),200
