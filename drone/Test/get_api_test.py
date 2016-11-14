7ccea79e83673b66610206c4fae409ab4dc54f63
iamrajhans <rajhans.jadhao@gmail.com>
2016-11-14 22:53:37 +0530

    added one test case

--- drone/Test/get_api_test.py
+++ drone/Test/get_api_test.py
@@ -1,4 +1,8 @@
 import pytest
+from drone.main import create_app
 
-def check_users_api():
-    ''
+def test_users_api():
+    app = create_app('local')
+    tester = app.test_client()
+    response = tester.get('/users')
+    assert response.status_code == 412
