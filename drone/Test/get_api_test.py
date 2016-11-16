7ef3d8ca8d8732a48aea4086d2efdecc54c03882
iamrajhans <rajhans.jadhao@gmail.com>
2016-11-16 22:35:58 +0530

    creating app globally for test

--- drone/Test/get_api_test.py
+++ drone/Test/get_api_test.py
@@ -1,8 +1,9 @@
 import unittest
 from drone.main import create_app
 
-def test_users_api():
-    app = create_app('local')
-    tester = app.test_client()
+app = create_app('local')
+tester = app.test_client()
+
+def test_status_code():
     response = tester.get('/users')
     assert response.status_code == 412
