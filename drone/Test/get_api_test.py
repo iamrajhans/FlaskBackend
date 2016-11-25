b90c52827253086835513f12d798576d88698055
iamrajhans <rajhans.jadhao@gmail.com>
2016-11-25 23:10:33 +0530

    created class for storing the test app configuration

--- drone/Test/get_api_test.py
+++ drone/Test/get_api_test.py
@@ -3,6 +3,15 @@ from drone.main import create_app
 app = create_app('local')
 tester = app.test_client()
 
+
+class APPCONFIG(object) :
+    ENV = 'testing'
+    USERNAME = 'test'
+    KEY = 'test'
+
+def get_app_config():
+    return  APPCONFIG
+
 def test_status_code():
     response = tester.get('/users')
     assert response.status_code == 412
