989d88474fdc8ad777ad6210cf74a97e42bf831a
iamrajhans <rajhans.jadhao@gmail.com>
2016-12-29 00:04:33 +0530

    added column created_at and updated_at

--- drone/models/user_request.py
+++ drone/models/user_request.py
@@ -6,6 +6,8 @@ class UserRequest(db.Model):
     id = db.Column(db.String(255),autoincrement=True)
     from_user = db.Column(db.String(255))
     request_type = db.Column(db.String(255))
+    created_at = db.Column(db.TIMESTAMP,nullable=True)
+    updated_at = db.Column(db.TIMESTAMP,nullable=True)
 
     def as_dict(self):
         return {c.id: datetime.strftime(getattr(self, c.id), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.id),datetime) else getattr(self, c.id) for c in self.__table__.columns}
