98a73500d59d37e4776e4e3892475e868f1d43da
iamrajhans <rajhans.jadhao@gmail.com>
2016-12-30 23:22:08 +0530

    made created_at column should not be null

--- drone/models/user.py
+++ drone/models/user.py
@@ -6,7 +6,7 @@ class UserModel(db.Model):
     id    = db.Column(db.Integer,primary_key=True,autoincrement=True)
     name  = db.Column(db.String(100),nullable=True)
     email = db.Column(db.String(150),nullable=True)
-    created_at = db.Column(db.TIMESTAMP , nullable=True)
+    created_at = db.Column(db.TIMESTAMP , nullable=False)
     updated_at = db.Column(db.TIMESTAMP,nullable=True)
     def as_dict(self):
         return {c.name: datetime.strftime(getattr(self, c.name), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.name),datetime) else getattr(self, c.name) for c in self.__table__.columns}
