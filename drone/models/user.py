0076b462d244194f485d077f91b3fccdc1e16eaa
iamrajhans <rajhans.jadhao@gmail.com>
2016-12-31 10:25:35 +0530

    made updated_at column nullable false

--- drone/models/user.py
+++ drone/models/user.py
@@ -7,7 +7,7 @@ class UserModel(db.Model):
     name  = db.Column(db.String(100),nullable=True)
     email = db.Column(db.String(150),nullable=True)
     created_at = db.Column(db.TIMESTAMP , nullable=False)
-    updated_at = db.Column(db.TIMESTAMP,nullable=True)
+    updated_at = db.Column(db.TIMESTAMP,nullable=False)
     def as_dict(self):
         return {c.name: datetime.strftime(getattr(self, c.name), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.name),datetime) else getattr(self, c.name) for c in self.__table__.columns}
 
