4f8f844ec6c47f0e6d03ebe74fc79429ec29827b
iamrajhans <rajhans.jadhao@gmail.com>
2017-01-03 22:59:06 +0530

    making nullable the created_at and updated_at

--- drone/models/user.py
+++ drone/models/user.py
@@ -10,8 +10,8 @@ class UserModel(db.Model):
     email = db.Column(db.String(150),nullable=True)
     birth_date = db.Column(db.TIMESTAMP,nullable=True)
     address = db.Column(db.String(255),nullable=True)
-    created_at = db.Column(db.TIMESTAMP , nullable=False)
-    updated_at = db.Column(db.TIMESTAMP,nullable=False)
+    created_at = db.Column(db.TIMESTAMP , nullable=True)
+    updated_at = db.Column(db.TIMESTAMP,nullable=True)
     def as_dict(self):
         return {c.name: datetime.strftime(getattr(self, c.name), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.name),datetime) else getattr(self, c.name) for c in self.__table__.columns}
 
