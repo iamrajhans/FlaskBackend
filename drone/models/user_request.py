b19dd668fe577d16a0d85018a1038089d1d32821
iamrajhans <rajhans.jadhao@gmail.com>
2017-01-09 22:46:49 +0530

    made some changes

--- drone/models/user_request.py
+++ drone/models/user_request.py
@@ -2,7 +2,7 @@ from drone.main import db
 from datetime import datetime
 
 class UserRequest(db.Model):
-    __tablename__ = 'referral'
+    __tablename__ = 'request_referral'
     id = db.Column(db.String(255),autoincrement=True)
     user_id = db.Column(db.String(100), nullable=True)
     from_user = db.Column(db.String(255),nullable=False)
