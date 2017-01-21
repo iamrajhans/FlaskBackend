6fa25e9dcfc2b84ecfd40cc74eb26a2695eea3a0
iamrajhans <rajhans.jadhao@gmail.com>
2017-01-22 00:26:36 +0530

    fixed issue of resolving particular node field

--- drone/models/schema.py
+++ drone/models/schema.py
@@ -24,14 +24,20 @@ class Query(ObjectType):
     node = relay.Node.Field(Users) # use to resolve the node field
 
     def resolve_user(self, args, context, info):
+        """
+
+        Args:
+            args: arguments which are pass through the user node
+            context: context of the query
+        filters : filtering  on the basis of the table column keys and checking that key(column name) in argument
+        Returns: if arguments are given and if that node is prsent then it is return
+
+        """
         query = Users.get_query(context)  # SQLAlchemy query
-        #filter query TODO : filter query in different way
-        if args and 'name' in args:
-            return query.filter(UserModel.name == args['name'])
-        if args and 'email' in args:
-            return query.filter(UserModel.email == args['email'])
-        else:
-            return query.all()
+        #filter query TODO : filter query in different way (Done)
+        for column_name in UserModel.__table__.columns.keys():
+            if column_name in args:
+                return query.filter(getattr(UserModel,column_name) == args.get(column_name))
 
 
 schema = Schema(query=Query)
