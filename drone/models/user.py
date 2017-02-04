664729341462d4428cca1637f1f07f5af6451d2b
iamrajhans <rajhans.jadhao@gmail.com>
2017-02-04 21:54:17 +0530

    hange in the user mode and made some mandtory field while saving the user data

--- drone/models/user.py
+++ drone/models/user.py
@@ -4,14 +4,14 @@ from datetime import datetime
 class UserModel(db.Model):
     __tablename__ = 'users'
     id    = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    user_id = db.Column(db.String(100),nullable=True)
-    name  = db.Column(db.String(100),nullable=True)
-    last_name = db.Column(db.String(200),nullable=True)
-    email = db.Column(db.String(150),nullable=True)
+    user_id = db.Column(db.String(100),nullable=False)
+    name  = db.Column(db.String(100),nullable=False)
+    last_name = db.Column(db.String(200),nullable=False)
+    email = db.Column(db.String(150),nullable=False)
     birth_date = db.Column(db.TIMESTAMP,nullable=True)
     address = db.Column(db.String(255),nullable=True)
-    created_at = db.Column(db.TIMESTAMP , nullable=True)
-    updated_at = db.Column(db.TIMESTAMP,nullable=True)
+    created_at = db.Column(db.TIMESTAMP , nullable=False)
+    updated_at = db.Column(db.TIMESTAMP,nullable=False)
     def as_dict(self):
         return {c.name: datetime.strftime(getattr(self, c.name), '%Y-%m-%d %H:%M:%S') if isinstance(getattr(self, c.name),datetime) else getattr(self, c.name) for c in self.__table__.columns}
 
