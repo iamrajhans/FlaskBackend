fafd7f172699ad5b839449a3f85fcd2e67af8a5f
iamrajhans <rajhans.jadhao@gmail.com>
2017-02-12 23:03:55 +0530

    app logging level is set to info

--- drone/config/common.py
+++ drone/config/common.py
@@ -1,19 +1,14 @@
 #include your common config to your app
 import logging
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+APP_LOG_LEVEL = logging.INFO
 SECRET_KEY ='secret key'
 
-
 DEBUG = True
-
-
 #------- Database URI ----------#
 SQLALCHEMY_DATABASE_URI= 'postgresql://username:password@localhost:5432/db_name'
 
-
 #-------- Sentry ------#
-
-
 DSN_SENTRY ='https://8bde72b5d31d49b497f1e49226ae0f96:71b0bc035d684846803e5e6ef725ca96@app.getsentry.com/94768'
